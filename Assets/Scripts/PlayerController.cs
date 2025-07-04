e3868cbcfe3ce6b9b186ced44bb39dbe16905c1f
Jose Angel <josealr97@gmail.com>
2025-07-04 18:36:48 -0500

    Track spline and following path initial behavior

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.Serialization;
+using UnityEngine.Splines;
 
 public class PlayerController : MonoBehaviour
 {
@@ -22,12 +23,17 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private Transform cam;
     [SerializeField] private Transform groundedPoint;
     [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private SplineContainer _splineContainer;
 
 //-----------------//Variables//-----------------//
 //Process variables - private
     private bool _isGrounded = false;
     private Vector3 adjustedDirection;
-
+    private bool _auto = false;
+    private Vector3 temp_spline;
+    private float _turnSmoothSpeed;
+    private float t = 0;
+    
 //Balance variables - serialized 
     [SerializeField] private float speed;
     [SerializeField] private float gravity;
@@ -35,22 +41,26 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float turnSmoothTime = 0.1f;
 
 //Public properties - private set "Name { get; private set; }"
-    private float _turnSmoothSpeed;
+
 
 //-----------------//Functions//-----------------//
 //Built-in
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.R)) {
+            _auto = !_auto;
+        }
+        
         //Ground check
         _isGrounded = Physics.CheckSphere(groundedPoint.position, groundRadius, groundLayer);
         if (!_isGrounded)
         {
             transform.Translate(Vector3.down * (gravity * Time.deltaTime));
         }
-        //PLayer input
+        //Player input
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
-        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+        Vector3 inputDirection = new Vector3(horizontal, 0f, vertical).normalized;
         
         //Slope logic
         Physics.Raycast(groundedPoint.position, Vector3.down, out var hit, 1f, groundLayer);
@@ -58,9 +68,9 @@ public class PlayerController : MonoBehaviour
         Quaternion slopeRotation = Quaternion.FromToRotation(Vector3.up, groundNormal);
         
         //Movement logic
-        if (direction.magnitude >= 0.1f)
+        if (!_auto && inputDirection.magnitude >= 0.1f)
         {
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float targetAngle = Mathf.Atan2(inputDirection.x, inputDirection.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref _turnSmoothSpeed, turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
             
@@ -74,6 +84,21 @@ public class PlayerController : MonoBehaviour
             
             transform.Translate(adjustedDirection * (speed * Time.deltaTime), Space.World);
         }
+
+        /*if (true) {
+            var localPoint = _splineContainer.transform.InverseTransformPoint(transform.position);
+            SplineUtility.GetNearestPoint(_splineContainer.Spline, localPoint, out var nearest, out var ratio, 10, 10);
+            
+            temp_spline = _splineContainer.Spline.EvaluatePosition(ratio);
+            Debug.Log(temp_spline);
+            
+            //temp_spline = nearest;
+            //Debug.Log(temp_spline);
+        }*/
+        t += Time.deltaTime / 5;
+        temp_spline = _splineContainer.Spline.EvaluatePosition(t);
+        temp_spline = _splineContainer.transform.TransformPoint(temp_spline);
+        t = t >= 1f ? 0f : t;
     }
 
     private void OnDrawGizmos()
@@ -84,6 +109,9 @@ public class PlayerController : MonoBehaviour
         
         Gizmos.color = Color.blue;
         Gizmos.DrawLine(transform.position, transform.position + adjustedDirection);
+        
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(temp_spline, 1.0f);
     }
 
 //Inner process - private
