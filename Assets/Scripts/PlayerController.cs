15136eefb10ac2ac663a389664cf1d50d512283b
Jose Angel <josealr97@gmail.com>
2025-07-05 01:47:06 -0500

    Autopilot for player that follows route spline

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -30,7 +30,7 @@ public class PlayerController : MonoBehaviour
     private bool _isGrounded = false;
     private Vector3 adjustedDirection;
     private bool _auto = false;
-    private Vector3 temp_spline;
+    private Vector3 railDirection;
     private float _turnSmoothSpeed;
     private float t = 0;
     
@@ -58,9 +58,12 @@ public class PlayerController : MonoBehaviour
             transform.Translate(Vector3.down * (gravity * Time.deltaTime));
         }
         //Player input
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
-        Vector3 inputDirection = new Vector3(horizontal, 0f, vertical).normalized;
+        Vector3 inputDirection = Vector3.zero;
+        if (!_auto) {
+            float horizontal = Input.GetAxisRaw("Horizontal");
+            float vertical = Input.GetAxisRaw("Vertical");
+            inputDirection = new Vector3(horizontal, 0f, vertical).normalized;
+        }
         
         //Slope logic
         Physics.Raycast(groundedPoint.position, Vector3.down, out var hit, 1f, groundLayer);
@@ -68,13 +71,34 @@ public class PlayerController : MonoBehaviour
         Quaternion slopeRotation = Quaternion.FromToRotation(Vector3.up, groundNormal);
         
         //Movement logic
-        if (!_auto && inputDirection.magnitude >= 0.1f)
-        {
-            float targetAngle = Mathf.Atan2(inputDirection.x, inputDirection.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+        Vector3 targetDirection = Vector3.zero;
+        if (_auto) {
+            //spline t
+            var localPoint = _splineContainer.transform.InverseTransformPoint(transform.position);
+            SplineUtility.GetNearestPoint(_splineContainer.Spline, localPoint, out _, out var ratio, 100, 5);
+            
+            Vector3 tangent = _splineContainer.Spline.EvaluateTangent(ratio);
+            Vector3 normalizedTangent = tangent.normalized;
+            railDirection = _splineContainer.transform.TransformDirection(normalizedTangent);
+            railDirection.y = 0;
+            targetDirection = railDirection;
+            
+            //Rotation
+            float targetAngle = Mathf.Atan2(1, 0) * Mathf.Rad2Deg + cam.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref _turnSmoothSpeed, turnSmoothTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
             
-            Vector3 targetDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            Debug.Log("inside auto");
+        }else {
+            float targetAngle = Mathf.Atan2(inputDirection.x, inputDirection.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref _turnSmoothSpeed, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+            targetDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            Debug.Log("inside not auto");
+        }
+        
+        if (inputDirection.magnitude >= 0.1f || _auto) {
+            
             adjustedDirection = slopeRotation * targetDirection;
 
             /*if (adjustedDirection.y < 0)
@@ -85,20 +109,8 @@ public class PlayerController : MonoBehaviour
             transform.Translate(adjustedDirection * (speed * Time.deltaTime), Space.World);
         }
 
-        /*if (true) {
-            var localPoint = _splineContainer.transform.InverseTransformPoint(transform.position);
-            SplineUtility.GetNearestPoint(_splineContainer.Spline, localPoint, out var nearest, out var ratio, 10, 10);
-            
-            temp_spline = _splineContainer.Spline.EvaluatePosition(ratio);
-            Debug.Log(temp_spline);
-            
-            //temp_spline = nearest;
-            //Debug.Log(temp_spline);
-        }*/
-        t += Time.deltaTime / 5;
-        temp_spline = _splineContainer.Spline.EvaluatePosition(t);
-        temp_spline = _splineContainer.transform.TransformPoint(temp_spline);
-        t = t >= 1f ? 0f : t;
+        
+        
     }
 
     private void OnDrawGizmos()
@@ -111,7 +123,7 @@ public class PlayerController : MonoBehaviour
         Gizmos.DrawLine(transform.position, transform.position + adjustedDirection);
         
         Gizmos.color = Color.red;
-        Gizmos.DrawSphere(temp_spline, 1.0f);
+        Gizmos.DrawLine(transform.position, transform.position + railDirection * 2f);
     }
 
 //Inner process - private
